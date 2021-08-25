52b4cb15da02dde8e452ef55c2e975cb05b42bb5
Eddie Bassey <eddie.bassey@nology.io>
2021-08-25 06:46:20 +0100

    added function to generate random num and event listener

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const messageEl = document.querySelector("#message-el");
 const cardEl = document.querySelector("#card-el");
-const showEl = document.querySelector("#show-el");
+const sumEl = document.querySelector("#sum-el");
+const startBtn = document.querySelector("#start-btn");
+const pickBtn = document.querySelector("#pick-btn");
+let cards = [];
+
+function generateNum() {
+	let randomNum = Math.floor(Math.random() * 13) + 1;
+	return randomNum;
+}
+
+startBtn.addEventListener("click", function () {
+	let firstNum = generateNum();
+	let secondNum = generateNum();
+	cards = [firstNum, secondNum];
+	for (let i = 0; i < cards.length; i++) {
+		cardEl.textContent += ` ${cards[i]}`;
+	}
+	sumEl.textContent += firstNum + secondNum;
+});
