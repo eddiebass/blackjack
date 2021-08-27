b2c536f5c1e426c5f5b0ebee4aeb725d86e23937
Eddie Bassey <eddie.bassey@nology.io>
2021-08-27 02:39:45 +0100

    added a new eventlistener to DOM manipulation

--- index.js
+++ index.js
@@ -4,18 +4,57 @@ const sumEl = document.querySelector("#sum-el");
 const startBtn = document.querySelector("#start-btn");
 const pickBtn = document.querySelector("#pick-btn");
 let cards = [];
+let message = "";
+let sum = 0;
+let isAlive = false;
+let hasBlackJack = false;
+//let firstNum = 0;
+//let secondNum = 0;
+//let thirdNum = 0;
 
 function generateNum() {
 	let randomNum = Math.floor(Math.random() * 13) + 1;
-	return randomNum;
+	if (randomNum === 1) {
+		return 11;
+	} else if (randomNum > 10) {
+		return 10;
+	} else {
+		return randomNum;
+	}
 }
 
 startBtn.addEventListener("click", function () {
+	isAlive = true;
 	let firstNum = generateNum();
 	let secondNum = generateNum();
 	cards = [firstNum, secondNum];
-	for (let i = 0; i < cards.length; i++) {
-		cardEl.textContent += ` ${cards[i]}`;
-	}
-	sumEl.textContent += firstNum + secondNum;
+	sum = firstNum + secondNum;
+	renderGame();
 });
+
+pickBtn.addEventListener("click", function () {
+	if (isAlive === true && hasBlackJack === false) {
+		let thirdNum = generateNum();
+		sum += thirdNum;
+		cards.push(thirdNum);
+		renderGame();
+	}
+});
+
+function renderGame() {
+	cardEl.textContent = "cards: ";
+	for (let i = 0; i < cards.length; i++) {
+		cardEl.textContent += `${cards[i]} `;
+	}
+	sumEl.textContent = sum;
+	if (sum === 21) {
+		message = "Wohoo! You've got Blackjack!";
+		hasBlackJack = true;
+	} else if (sum < 21) {
+		message = "Draw another card";
+	} else {
+		message = "Game Over!, you are out of the game!";
+		isAlive = false;
+	}
+	messageEl.textContent = message;
+}
